51d9d4eeb809aefc45c272850df63441a7d1df81
RSWilli <bartel.wilhelm@gmail.com>
2023-11-09 23:07:12 +0100

    use flake-parts, add agenix and add devshells

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
 
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.05";
 
+    agenix.url = "github:ryantm/agenix";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -18,25 +20,91 @@
 
     nixos-hardware.url = "github:nixos/nixos-hardware/master";
 
-    # devshell.url = "github:numtide/devshell";
+    devshell.url = "github:numtide/devshell";
+    # flake-utils.url = "github:numtide/flake-utils";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs = {
     self,
     nixpkgs,
+    agenix,
+    devshell,
+    flake-parts,
     ...
-  } @ inputs: {
-    # formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        devshell.flakeModule
+      ];
 
-    nixosConfigurations = {
-      think = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./systems/think
-        ];
+      flake = {
+        nixosConfigurations = {
+          think = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            specialArgs = {inherit inputs;};
+            modules = [
+              ./systems/think
+              agenix.nixosModules.default
+            ];
+          };
+        };
+      };
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+        devshells.default = {
+          commands = [
+            # {
+            #   name = "agenix";
+            #   help = "wrapper around agenix";
+            #   command = ''
+            #     sudo EDITOR="${pkgs.lib.getExe pkgs.vim}" ${pkgs.lib.getExe' inputs'.agenix.packages.default "agenix"} --identity /etc/ssh/ssh_host_ed25519_key "$@"
+            #   '';
+            # }
+            {
+              name = "apply";
+              help = "wrapper for nixos-rebuild switch";
+              command = "sudo nixos-rebuild switch --flake . $@";
+            }
+            {
+              name = "test";
+              help = "wrapper for nixos-rebuild switch";
+              command = "sudo nixos-rebuild test --flake . $@";
+            }
+            {
+              name = "boot";
+              help = "wrapper for nixos-rebuild switch";
+              command = "sudo nixos-rebuild boot --flake . $@";
+            }
+          ];
+          packages = with pkgs; [];
+        };
+        devshells.install = {
+          commands = [
+            {
+              name = "format";
+              help = "format disk according to config";
+              # sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes run github:nix-community/disko -- --flake github:rswilli/nixos#think --mode disko
+              command = "sudo ${pkgs.lib.getExe' inputs'.disko.packages.default "disko"} --flake .#$1 --mode disko";
+            }
+            {
+              name = "install";
+              help = "install system";
+              command = "sudo nixos-install --flake .#$1";
+            }
+          ];
+        };
       };
     };
-  };
 }

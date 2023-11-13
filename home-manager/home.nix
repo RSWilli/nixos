dd9e597184bfd36eabca8a19f6f6fe18b497721a
RSWilli <bartel.wilhelm@gmail.com>
2023-11-13 23:44:19 +0100

    add direnv

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -39,6 +39,11 @@
     };
   };
 
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 

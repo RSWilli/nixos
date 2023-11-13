42337ad7efda7aba463bc37f10ba70921ba03a98
RSWilli <bartel.wilhelm@gmail.com>
2023-11-13 23:51:27 +0100

    switch to latest kernel for think

--- nixos/system.nix
+++ nixos/system.nix
@@ -1,10 +1,6 @@
 {
   imports = [];
 
-  # maybe use a newer linux kernel?
-  # https://nixos.wiki/wiki/Linux_kernel
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
--- systems/think/default.nix
+++ systems/think/default.nix
@@ -21,6 +21,10 @@
     ../../disko/encrypted.nix
   ];
 
+  # use a newer linux kernel
+  # https://nixos.wiki/wiki/Linux_kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   my.root-disk = "/dev/nvme0n1";
 
   networking.hostName = "think";

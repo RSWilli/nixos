bb4654ff066e4938d6b6fe009e8f3b62b4d9112d
RSWilli <bartel.wilhelm@gmail.com>
2024-01-04 10:13:08 +0100

    rekeyed the secrets against the installer key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,17 @@
 let
+  # this key is used so we do not need root access when rekeying
   willi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGTU+w/41oNnBH7Zf9qa5PIREYSzlAwRkdvYpym5BF4j";
 
   think = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDseXq0mgC/CN9CtgZeduV7MG1h0wzohUS+Cy+hzRMBz";
-  main = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICXdxXHZ/sJEj19V7o4at7YOLElpLs4ZhH7hRiiJCbqn";
+  # main = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICXdxXHZ/sJEj19V7o4at7YOLElpLs4ZhH7hRiiJCbqn";
 
-  systems = [main think];
+  # key to use when installing a new system;
+  install = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIItCGHfgqEe0PfDME+cZsH5g/D2ZX9+VFxNS+HT94hEV";
+
+  systems = [install think];
   users = [willi];
   all = systems ++ users;
 in {
   "password.age".publicKeys = all;
-  "root-password.age".publicKeys = systems;
+  "root-password.age".publicKeys = all;
 }

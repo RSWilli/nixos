36cacc2f23c0dbbc851fc0a3628fdebc0746f0ec
RSWilli <bartel.wilhelm@gmail.com>
2024-01-05 16:32:08 +0100

    run nix fmt

--- shell.nix
+++ shell.nix
@@ -1,11 +1,15 @@
 # source: https://github.com/edolstra/flake-compat
-(import
+(
+  import
   (
-    let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-    fetchTarball {
-      url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
+    let
+      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+    in
+      fetchTarball {
+        url = lock.nodes.flake-compat.locked.url or "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+        sha256 = lock.nodes.flake-compat.locked.narHash;
+      }
   )
-  { src = ./.; }
-).shellNix
+  {src = ./.;}
+)
+.shellNix

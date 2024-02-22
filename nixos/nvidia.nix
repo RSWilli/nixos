c90a0884f8d15680c09affd5320b5653986f45cc
RSWilli <bartel.wilhelm@gmail.com>
2024-02-22 22:07:28 +0100

    switch to stable nvidia

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -39,8 +39,8 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
   };
 
   boot.blacklistedKernelModules = [ "nouveau" ];

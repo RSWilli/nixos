d7eba9ea438e94d759df1b695191df9d9738d371
RSWilli <bartel.wilhelm@gmail.com>
2024-03-13 20:55:01 +0100

    formating

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -15,11 +15,11 @@
   services.xserver.videoDrivers = ["nvidia"];
 
   hardware.nvidia = {
-    modesetting.enable = true; 
-    powerManagement.enable = false; 
-    powerManagement.finegrained = false; 
-    open = true; 
-    nvidiaSettings = true; 
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 

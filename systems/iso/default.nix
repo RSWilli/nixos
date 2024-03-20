e1370bc485c1ac385eec30e6f08050fe68242d88
RSWilli <bartel.wilhelm@gmail.com>
2024-03-20 20:14:43 +0100

    remove everything that could expose secrets in custom ISO

--- systems/iso/default.nix
+++ systems/iso/default.nix
@@ -9,15 +9,8 @@
 }: {
   imports = [
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
-
-    ../../nixos/gnome.nix
     ../../nixos/i18n.nix
     ../../nixos/nix.nix
-
-    #  dont use system.nix, as the installer will configure conflicting settings
-    # ../../nixos/system.nix
-    ../../nixos/users.nix
-    inputs.home-manager.nixosModules.home-manager
   ];
 
   # https://nixos.wiki/wiki/Linux_kernel
@@ -27,11 +20,4 @@
   nixpkgs.hostPlatform = "x86_64-linux";
   environment.systemPackages = [];
   isoImage.squashfsCompression = "gzip -Xcompression-level 1";
-
-  services.getty.autologinUser = lib.mkForce "willi";
-
-  networking.networkmanager.enable = false;
-
-  # override root initial password set by the installer
-  users.users.root.initialHashedPassword = lib.mkForce null;
 }

d45a15da88f84a9271b12e2e9cf6f9e25d6e4aa5
RSWilli <bartel.wilhelm@gmail.com>
2024-06-23 13:25:21 +0200

    formatting

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -1,15 +1,17 @@
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
   # user need to run
   # sudo tailscale up --accept-routes
   # to start using tailscale
 
-
   # make the tailscale command usable to users
-  environment.systemPackages = [ pkgs.tailscale ];
-  
+  environment.systemPackages = [pkgs.tailscale];
+
   services.tailscale.useRoutingFeatures = "client";
-  
+
   # enable the tailscale service
   services.tailscale.enable = true;
 }

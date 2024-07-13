4b8dc462d1f2b2ebd81e70bbe4f10bfc6690e7fb
RSWilli <bartel.wilhelm@gmail.com>
2024-07-13 20:55:04 +0200

    add paperless config for server, WIP

---
+++ nixos/server/paperless.nix
@@ -0,0 +1,19 @@
+{ lib, config, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.my.paperless;
+in
+
+{
+  options.my.paperless = {
+    enable = mkEnableOption "paperless";
+  };
+
+  config = mkIf cfg.enable {
+    services.paperless = {
+      enable = true;
+    };
+  };
+}

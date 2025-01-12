da2137b3c843e854a68c07e16eeaccaed03f5807
RSWilli <bartel.wilhelm@gmail.com>
2025-01-12 19:19:09 +0100

    update and ROCm stuff

--- nixos/desktop/amd.nix
+++ nixos/desktop/amd.nix
@@ -13,17 +13,30 @@ in {
     hardware.graphics.extraPackages = [
       # this causes black boxes https://gitlab.gnome.org/GNOME/gtk/-/issues/6890 , re-enable when fixed
       # pkgs.amdvlk
-      pkgs.rocmPackages.clr.icd # rocM
+
+      # ROCm
+      pkgs.rocmPackages.clr.icd
     ];
+
+    nixpkgs.config.rocmSupport = true;
 
     boot.kernelModules = ["kvm-amd"];
     boot.initrd.kernelModules = ["amdgpu"];
-
-    # services.xserver.videoDrivers = ["amdgpu"];
 
     environment.systemPackages = with pkgs; [
       radeontop
       clinfo
     ];
+
+    # fix ROCm path for software where it is hardcoded
+    systemd.tmpfiles.rules = [
+      "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.rocmPackages.clr}"
+    ];
+
+    environment.sessionVariables = {
+      # Radeon RX 7800XT needs for ROCm 11.0.0 as per: https://llvm.org/docs/AMDGPUUsage.html
+      # defined by the gfx1100
+      HSA_OVERRIDE_GFX_VERSION = "11.0.0";
+    };
   };
 }

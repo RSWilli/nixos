c54f034d1d0f358374c948737be32a409fb745c3
RSWilli <bartel.wilhelm@gmail.com>
2025-10-17 22:28:24 +0200

    update and set more steam options

--- nixos/desktop/steam.nix
+++ nixos/desktop/steam.nix
@@ -14,9 +14,15 @@ in {
   config = mkIf cfg.enable {
     programs.steam = {
       enable = true;
-      # remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      # dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
       gamescopeSession.enable = true;
+
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
     };
 
     environment.systemPackages = [

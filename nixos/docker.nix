a7bc0118616ae3dbd47fe9c23ed9ff4756c96cee
RSWilli <bartel.wilhelm@gmail.com>
2025-10-26 17:18:51 +0100

    fix docker for WIFIonICE

--- nixos/docker.nix
+++ nixos/docker.nix
@@ -16,6 +16,13 @@ in {
 
       # automatically run a prune schedule
       autoPrune.enable = true;
+
+      daemon.settings = {
+        #  WIFIonICE vs Docker: Fixing DB (Deutsche Bahn) WIFI by moving Docker away from 172.18.x.x in /etc/docker/daemon.json
+        # source: https://gist.github.com/sunsided/7840e89ff4e11b64a2d7503fafa0290c
+        "bip" = "172.39.1.5/24";
+        "fixed-cidr" = "172.39.1.0/25";
+      };
     };
 
     users.users.willi.extraGroups = ["docker"];

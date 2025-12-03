011610c62f29413534a396748029397c189c34ed
RSWilli <bartel.wilhelm@gmail.com>
2025-12-03 11:24:08 +0100

    fix if statement so that steam is not installed on dell

--- nixos/desktop/steam.nix
+++ nixos/desktop/steam.nix
@@ -11,7 +11,7 @@ in {
     steam = mkEnableOption "steam";
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.steam {
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play

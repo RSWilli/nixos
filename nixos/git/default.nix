4906712fb8fd90aa5a8dd4d32c976dd760ce4c71
RSWilli <bartel.wilhelm@gmail.com>
2025-12-03 09:52:03 +0100

    add work gitconfig to commit with a different username

--- nixos/git/default.nix
+++ nixos/git/default.nix
@@ -1,18 +1,37 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   programs.git = {
     enable = true;
     lfs.enable = true;
     prompt.enable = true;
 
-    config = {
-      user.name = "RSWilli";
-      user.email = "bartel.wilhelm@gmail.com";
-      pull.rebase = "true";
-      push.followTags = "true";
+    config = [
+      {
+        user.name = "RSWilli";
+        user.email = "bartel.wilhelm@gmail.com";
+        pull.rebase = "true";
+        push.followTags = "true";
 
-      alias = {
-        pfush = "push origin --force-with-lease";
-      };
+        alias = {
+          pfush = "push origin --force-with-lease";
+        };
+      }
+      {
+        # needs to be after the user config, so in a new section
+        "includeIf \"gitdir:work/\"".path = "/etc/${config.environment.etc.gitconfig-work.target}";
+      }
+    ];
+  };
+
+  environment.etc.gitconfig-work = let
+    workConfig = {
+      user.name = "Wilhelm Bartel";
+      user.email = "wilhelm.bartel@streamonkey.de";
     };
+  in {
+    text = lib.generators.toGitINI workConfig;
   };
 }

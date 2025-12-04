775e5ed34b730335c57c1424926c2545205d590d
RSWilli <bartel.wilhelm@gmail.com>
2025-12-04 14:43:10 +0100

    try to fix wifi issues for Mediatek network card

--- systems/dell/default.nix
+++ systems/dell/default.nix
@@ -8,6 +8,16 @@
   # boot.kernelPackages = pkgs.linuxPackages; # latest LTS kernel
   # boot.kernelPackages = pkgs.linuxPackages_zen; # zen kernel, patched for everyday desktop performance
 
+  boot.kernelParams = [
+    # c2:00.0 Network controller: MEDIATEK Corp. Device 7925
+    # Subsystem: Foxconn International, Inc. Device e137
+    # Kernel driver in use: mt7925e
+    # Kernel modules: mt7925e
+    #
+    # disable power management to avoid connectivity issues
+    "mt7925e.disable_aspm=1"
+  ];
+
   my = {
     disko = {
       root-disk = "/dev/nvme0n1";

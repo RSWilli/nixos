1efcdcee4d7b0a028244a95247b064471da8ceb5
RSWilli <bartel.wilhelm@gmail.com>
2025-12-08 11:03:00 +0100

    more mediatek wifi workarounds

--- systems/dell/default.nix
+++ systems/dell/default.nix
@@ -50,6 +50,24 @@
   # powertop and auto tune service
   powerManagement.powertop.enable = true;
 
+  # try to fix MediaTek wifi issues
+  networking.networkmanager.wifi.powersave = false;
+
+  # use newer linux-firmware for to fix wifi issues
+  nixpkgs.overlays = [
+    (self: super: {
+      linux-firmware = super.linux-firmware.overrideAttrs (oldAttrs: {
+        version = "willi-custom1";
+        src = super.fetchFromGitLab {
+          owner = "kernel-firmware";
+          repo = "linux-firmware";
+          rev = "536cc58d9db164b0e2a47f7f33996b0cf194691a";
+          hash = "sha256-cXsLhF8+5cjRf/cq0yJiNX4midGtotd4XBB7lw7mkBU=";
+        };
+      });
+    })
+  ];
+
   # BIOS updates, run `fwupdmgr update` to check for updates and install them
   # https://wiki.nixos.org/wiki/Fwupd
   services.fwupd.enable = true;

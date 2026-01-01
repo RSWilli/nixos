caccae8c87e7e51f767ffd679a2c4fe193a9b312
RSWilli <bartel.wilhelm@gmail.com>
2026-01-01 16:35:15 +0100

    remove linux firmware workarounds for mediatek wifi on dell in hope that a newer kernel will fix it

--- systems/dell/default.nix
+++ systems/dell/default.nix
@@ -8,15 +8,15 @@
   # boot.kernelPackages = pkgs.linuxPackages; # latest LTS kernel
   # boot.kernelPackages = pkgs.linuxPackages_zen; # zen kernel, patched for everyday desktop performance
 
-  boot.kernelParams = [
-    # c2:00.0 Network controller: MEDIATEK Corp. Device 7925
-    # Subsystem: Foxconn International, Inc. Device e137
-    # Kernel driver in use: mt7925e
-    # Kernel modules: mt7925e
-    #
-    # disable power management to avoid connectivity issues
-    "mt7925e.disable_aspm=1"
-  ];
+  # boot.kernelParams = [
+  #   # c2:00.0 Network controller: MEDIATEK Corp. Device 7925
+  #   # Subsystem: Foxconn International, Inc. Device e137
+  #   # Kernel driver in use: mt7925e
+  #   # Kernel modules: mt7925e
+  #   #
+  #   # disable power management to avoid connectivity issues
+  #   "mt7925e.disable_aspm=1"
+  # ];
 
   my = {
     disko = {
@@ -51,22 +51,7 @@
   powerManagement.powertop.enable = true;
 
   # try to fix MediaTek wifi issues
-  networking.networkmanager.wifi.powersave = false;
-
-  # use newer linux-firmware for to fix wifi issues
-  nixpkgs.overlays = [
-    (self: super: {
-      linux-firmware = super.linux-firmware.overrideAttrs (oldAttrs: {
-        version = "willi-custom1";
-        src = super.fetchFromGitLab {
-          owner = "kernel-firmware";
-          repo = "linux-firmware";
-          rev = "536cc58d9db164b0e2a47f7f33996b0cf194691a";
-          hash = "sha256-cXsLhF8+5cjRf/cq0yJiNX4midGtotd4XBB7lw7mkBU=";
-        };
-      });
-    })
-  ];
+  # networking.networkmanager.wifi.powersave = false;
 
   # BIOS updates, run `fwupdmgr update` to check for updates and install them
   # https://wiki.nixos.org/wiki/Fwupd

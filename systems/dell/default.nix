47fa132bf5766ae69f5eef4ce0e60ca5704f9d5d
RSWilli <bartel.wilhelm@gmail.com>
2026-01-12 10:53:34 +0100

    blacklist mediatek wifi kernel module

--- systems/dell/default.nix
+++ systems/dell/default.nix
@@ -13,15 +13,14 @@
   # boot.kernelPackages = pkgs.linuxPackages; # latest LTS kernel
   # boot.kernelPackages = pkgs.linuxPackages_zen; # zen kernel, patched for everyday desktop performance
 
-  # boot.kernelParams = [
-  #   # c2:00.0 Network controller: MEDIATEK Corp. Device 7925
-  #   # Subsystem: Foxconn International, Inc. Device e137
-  #   # Kernel driver in use: mt7925e
-  #   # Kernel modules: mt7925e
-  #   #
-  #   # disable power management to avoid connectivity issues
-  #   "mt7925e.disable_aspm=1"
-  # ];
+  # c2:00.0 Network controller: MEDIATEK Corp. Device 7925
+  # Subsystem: Foxconn International, Inc. Device e137
+  # Kernel driver in use: mt7925e
+  # Kernel modules: mt7925e
+  #
+  # constantly drops wifi connection speed. I have an alternative usb wifi adapter.
+  # disable the module entirely until a fix is found.
+  boot.blacklistedKernelModules = ["mt7925e"];
 
   my = {
     disko = {

bd78f295b6695957bde4ea2cdbbd3fe7d73e6095
RSWilli <bartel.wilhelm@gmail.com>
2026-01-14 17:16:03 +0100

    remove kernel module blacklists because linux-firmware update contains new wifi firmware for mt7925

--- systems/dell/default.nix
+++ systems/dell/default.nix
@@ -13,16 +13,6 @@
   # boot.kernelPackages = pkgs.linuxPackages; # latest LTS kernel
   # boot.kernelPackages = pkgs.linuxPackages_zen; # zen kernel, patched for everyday desktop performance
 
-  # c2:00.0 Network controller: MEDIATEK Corp. Device 7925
-  # Subsystem: Foxconn International, Inc. Device e137
-  # Kernel driver in use: mt7925e
-  # Kernel modules: mt7925e
-  #
-  # constantly drops wifi connection speed. I have an alternative usb wifi adapter.
-  # disable the module entirely until a fix is found.
-  boot.blacklistedKernelModules = ["mt7925e"];
-  boot.extraModprobeConfig = "blacklist mt7925e";
-
   my = {
     disko = {
       root-disk = "/dev/nvme0n1";
@@ -56,9 +46,6 @@
 
   # powertop and auto tune service
   powerManagement.powertop.enable = true;
-
-  # try to fix MediaTek wifi issues
-  # networking.networkmanager.wifi.powersave = false;
 
   # BIOS updates, run `fwupdmgr update` to check for updates and install them
   # https://wiki.nixos.org/wiki/Fwupd

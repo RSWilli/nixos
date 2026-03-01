0f94ea59936497f8025599f1c326fda32059cc4e
RSWilli <bartel.wilhelm@gmail.com>
2026-03-01 16:40:40 +0100

    re-enable teamspeak for main

--- nixos/desktop/comms/default.nix
+++ nixos/desktop/comms/default.nix
@@ -28,14 +28,12 @@ in {
         ];
       }
     )
-    # qtwebengine-5.15.19 is marked as insecure (see https://github.com/NixOS/nixpkgs/issues/440030) and needs upstream fixes
-    # or nixpkgs.config.permittedInsecurePackages = [ "qtwebengine-5.15.19" ]; to be used
-    # (
-    #   mkIf cfg.teamspeak {
-    #     environment.systemPackages = [
-    #       pkgs.teamspeak_client
-    #     ];
-    #   }
-    # )
+    (
+      mkIf cfg.teamspeak {
+        environment.systemPackages = [
+          pkgs.teamspeak6-client
+        ];
+      }
+    )
   ];
 }

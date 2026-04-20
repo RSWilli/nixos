a6ba842fc88c2e20ecae848c45ab67de4d784ef8
RSWilli <bartel.wilhelm@gmail.com>
2026-04-20 16:39:54 +0200

    add some general tools to the environment

--- nixos/general.nix
+++ nixos/general.nix
@@ -3,7 +3,14 @@
     htop
     lm_sensors # for the `sensors` command
     numbat # terminal unit converter and calculator
+
+    # utility tools, replacements for some coreutils
+    ripgrep # search files in folder, see https://github.com/burntsushi/ripgrep
+    fd # find files with `fd <pattern>`, see https://github.com/sharkdp/fd
+    fzf # TUI for searching current directory, see https://github.com/junegunn/fzf
   ];
+
+  programs.fzf.fuzzyCompletion = true;
 
   # enforce that all users are configured via this flake
   users.mutableUsers = false;
